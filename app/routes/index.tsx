2ab696f0fcc6426f843ebe6be7c7c42858075d55
Mehul Mohan <mehulmpt@gmail.com>
2022-05-07 03:32:43 +0530

    init commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+import ReactMarkdown from 'react-markdown'
+
+export default function Index() {
+	return (
+		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+			<ReactMarkdown>Hello world</ReactMarkdown>
+		</div>
+	)
+}
